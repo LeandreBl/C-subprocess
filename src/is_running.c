5c74f3b1a90bd3e186574c31c651f3653e068e6c
Leandre <leandre.blanchard@epitech.eu>
2019-01-21 02:50:59 +0100

    unit tests

--- src/is_running.c
+++ src/is_running.c
@@ -5,6 +5,6 @@ bool lsubprocess_is_running(lsubprocess_t *subprocess)
   int status;
   pid_t pid = waitpid(subprocess->pid, &status, WNOHANG);
 
-  subprocess->is_running = (pid != 0);
+  subprocess->is_running = (pid == subprocess->pid);
   return (subprocess->is_running);
 }
