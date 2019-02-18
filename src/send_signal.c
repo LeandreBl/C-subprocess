14a4f2e1b4b7cc328ef31d43b836bae6ac657175
Leandre <leandre.blanchard@epitech.eu>
2019-02-18 15:49:40 +0100

    [add] send_signal to kill a signal to the subprocess

---
+++ src/send_signal.c
@@ -0,0 +1,10 @@
+#include <signal.h>
+
+#include "lsubprocess.h"
+
+int lsubprocess_send_signal(lsubprocess_t *subprocess, int signum)
+{
+  if (subprocess->is_running == true)
+    return (kill(subprocess->pid, signum));
+  return (-1);
+}
